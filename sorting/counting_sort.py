32eaf2c75af2e65956b2576d62582470162fad28
Roy Luo <r.luo2418@gmail.com>
2020-08-15 17:00:06 +1200

    adding counting sort for tuples

--- sorting/counting_sort.py
+++ sorting/counting_sort.py
@@ -1,3 +1,4 @@
+"""Stable counting sort algorithms""" 
 
 def counting_sort(lst: list) -> list: 
     """Sorts a given list of positive integers into ascending order"""
@@ -43,5 +44,42 @@ def check_all_integers(lst: list) -> bool:
     """Checks if list is a list of integers""" 
     for item in lst: 
         if not isinstance(item, int): 
+            return False 
+    return True
+
+def counting_sort_tuples(lst: list) -> list: 
+    """Sorts a list of tuples, where the first item represents the sorting value, into ascending order"""
+    n = len(lst) 
+    largest_value = find_max_tuple(lst) 
+    sorted_list = [None]*n 
+    count_list = [0]*largest_value
+    position_list = [0]*largest_value
+    # setting up count_list 
+    for item in lst: 
+        index = item[0] - 1 
+        count_list[index] += 1 
+    # setting up position_list 
+    for i in range(1, len(position_list)): 
+        position_list[i] = position_list[i - 1] + count_list[i - 1]
+    # inserting to output list 
+    for item in lst:
+        index = item[0] - 1 
+        sorted_list[position_list[index]] = item
+        position_list[index] += 1
+    return sorted_list
+
+
+def find_max_tuple(lst: list) -> int:  
+    """finds biggest sorting value in tuple (assuming sorting value is first item of tuple""" 
+    max = lst[0][0] 
+    for item in lst: 
+        if item[0] > max: 
+            max = item[0]
+    return max 
+
+def check_valid_tuple_list(lst: list) -> bool: 
+    """checks that tuples in list are valid: the sorting values (first item in typle) are positive integers""" 
+    for item in lst: 
+        if not isinstance(item[0], int) or item[0] <= 0: 
             return False 
     return True
